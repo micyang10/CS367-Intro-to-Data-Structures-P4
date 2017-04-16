b5d89223a310416894d88a5d09cc033f09afac8e
micyang10 <yang363@wisc.edu>
2017-04-16 02:14:23 -0500

    Update IntervalTree.java

--- IntervalTree.java
+++ IntervalTree.java
@@ -56,15 +56,84 @@ public class IntervalTree<T extends Comparable<T>> implements IntervalTreeADT<T>
 	@Override
 	public void delete(IntervalADT<T> interval)
 					throws IntervalNotFoundException, IllegalArgumentException {
-		// TODO Auto-generated method stub
-
+		if (interval == null) {
+			throw new IllegalArgumentException();
+		}
+		root = deleteHelper(root, interval);
 	}
 
 	@Override
 	public IntervalNode<T> deleteHelper(IntervalNode<T> node,
 					IntervalADT<T> interval)
 					throws IntervalNotFoundException, IllegalArgumentException {
-		// TODO Auto-generated method stub
+		if (interval == null) {
+			throw new IllegalArgumentException();
+		}
+		if (node == null) {
+			throw new IntervalNotFoundException(interval.toString());
+		}
+		if (node.getLeftNode().compareTo(interval) == 0) {
+			if (node.getLeftNode() == null && node.getRightNode() == null) {
+				return null;
+			}
+			else if (node.getLeftNode() != null && node.getRightNode() == null) {
+				return node.getLeftNode();
+			}
+			else if (node.getLeftNode() == null && node.getRightNode() != null) {
+				return node.getRightNode();
+			}
+			else {
+				node.setInterval(node.getSuccessor().getInterval());
+				deleteHelper(node.getRightNode(), node.getInterval());
+			}
+		}
+		else if (node.getInterval().compareTo(interval) > 0) {
+			node.setLeftNode(deleteHelper(node.getLeftNode(), interval));
+		}
+		else if (node.getInterval().compareTo(interval) < 0) {
+			node.setRightNode(deleteHelper(node.getRightNode(), interval));
+		}
+		if (node.getMaxEnd().compareTo(interval.getEnd()) == 0) {
+			T end = node.getInterval().getEnd();
+			if (node.getLeftNode() == null && node.getRightNode() == null) {
+				node.setMaxEnd(end);
+			}
+			else if (node.getLeftNode() != null && node.getRightNode() == null) {
+				if (node.getLeftNode().getMaxEnd().compareTo(end) > 0) {
+					node.setMaxEnd(node.getLeftNode().getMaxEnd());
+				}
+				else {
+					node.setMaxEnd(end);
+				}
+			}
+			else if (node.getLeftNode() == null && node.getRightNode() != null) {
+				if (node.getRightNode().getMaxEnd().compareTo(end) > 0) {
+					node.setMaxEnd(node.getRightNode().getMaxEnd());
+				}
+				else {
+					node.setMaxEnd(end);
+				}
+			}
+			else {
+				if (node.getLeftNode().getMaxEnd().compareTo(end) > 0) {
+					if (node.getLeftNode().getMaxEnd().compareTo
+					    (node.getRightNode().getMaxEnd()) > 0) {
+						node.setMaxEnd(node.getLeftNode().getMaxEnd());
+					}
+					else {
+						node.setMaxEnd(node.getRightNode().getMaxEnd());
+					}
+				}
+				else {
+					if (node.getRightNode().getMaxEnd().compareTo(end) > 0) {
+						node.setMaxEnd(node.getRightNode().getMaxEnd());
+					}
+					else {
+						node.setMaxEnd(end);
+					}
+				}
+			}
+		}
 	}
 
 	@Override
