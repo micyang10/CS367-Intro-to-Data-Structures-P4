79a148f662780a1c97b1b2fc4e382a1091faa06c
KendraRaczek <raczek@wisc.edu>
2017-04-16 20:48:15 -0500

    Update Interval.java

--- Interval.java
+++ Interval.java
@@ -20,75 +20,131 @@
  */
 public class Interval<T extends Comparable<T>> implements IntervalADT<T> {
 
-    T start;
-    T end;
-    String label;
+	// start date of an assignment
+	T start;
+	// end date of an assignment
+	T end;
+	// name of assignment
+	String label;
 
-    public Interval(T start, T end, String label) {
-        this.start = start;
-        this.end = end;
-        this.label = label;
-    }
+	/**
+	* Constructs new Interval instance, initializing start date, end date, and
+	* label fields from parameters.
+	* 
+	* @param start first date the assignment is available
+	* @param end last date the assignment is available
+	* @param label the name of the assignment
+	* @return newly constructed Interval object
+	*/
+	public Interval(T start, T end, String label) {
+		this.start = start;
+		this.end = end;
+		this.label = label;
+	}
 
-    @Override
-    public T getStart() {
-        return start;
-    }
+	/**
+	* Accesses start date of an Interval object
+	* 
+	* @return start date
+	*/
+	@Override
+	public T getStart() {
+		return start;
+	}
 
-    @Override
-    public T getEnd() {
-        return end;
-    }
+	/**
+	* Accesses end date of an Interval object
+	* 
+	* @return end date
+	*/
+	@Override
+	public T getEnd() {
+		return end;
+	}
+    
+	/**
+	* Accesses label of an Interval assignment
+	* 
+	* @return assignment label
+	*/
+	@Override
+	public String getLabel() {
+		return label;
+	}
 
-    @Override
-    public String getLabel() {
-        return label;
-    }
+	/**
+	* Compares two Intervals and checks whether or not the span of dates overlap
+	* each other
+	* 
+	* @param other the other Interval object that is being compared
+	* @return boolean value whether or not the two Intervals overlap
+	*/
+	@Override
+	public boolean overlaps(IntervalADT<T> other) {
+        	if (this.getStart().compareTo(other.getEnd()) <= 0 
+           		&& this.getEnd().compareTo(other.getStart()) >= 0) {
+            		return true;
+        	}
+        	return false;
+	}
 
-    @Override
-    public boolean overlaps(IntervalADT<T> other) {
-        if (this.getStart().compareTo(other.getEnd()) <= 0 
-           && this.getEnd().compareTo(other.getStart()) >= 0) {
-            return true;
-        }
-        return false;
-    }
+	/**
+	 * Checks whether a certain date point lies within the Interval period
+	 * 
+	 * @param point the date to check 
+	 * @return boolean value whether or not the point lies within the Interval
+	 */
+	@Override
+	public boolean contains(T point) {
+        	if (this.getEnd().compareTo(point) >= 0 && 
+        		this.getStart().compareTo(point) <= 0) {
+            		return true;
+        	}
+        	return false;
+    	}
 
-    @Override
-    public boolean contains(T point) {
-        if (this.getEnd().compareTo(point) >= 0 && this.getStart().compareTo(point) <= 0) {
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public int compareTo(IntervalADT<T> other) {
-        // first compares start values
-    	if (this.getStart().compareTo(other.getStart()) < 0) {
-    		return -1;
-    	} 
-		else if (this.getStart().compareTo(other.getStart()) > 0) {
+	/**
+	 * Compares two Intervals and assesses which occurs first chronologically.
+	 * Intervals are first compared by their start dates, and then their end 
+	 * dates if start dates are equal.
+	 * 
+	 * @param other the second interval to which compare this interval with
+	 * @return -1 if this interval's comes before the other interval, 
+	 * 1 if this interval comes after the other interval,
+	 * and 0 if the intervals are the same.
+	 */
+	@Override
+	public int compareTo(IntervalADT<T> other) {
+    		// first compares start values
+		if (this.getStart().compareTo(other.getStart()) < 0) {
+			return -1;
+		} else if (this.getStart().compareTo(other.getStart()) > 0) {
 			return 1;
 		}
 		
 		// if start values equal, then compares end values
 		else if (this.getEnd().compareTo(other.getEnd()) < 0) {
 			return -1;
-		} 
-		else if (this.getEnd().compareTo(other.getEnd()) > 0) {
+		} else if (this.getEnd().compareTo(other.getEnd()) > 0) {
 			return 1;
 		}
 		
 		// if both start and end values equal
 		else {
 			return 0;
-		}
-    }
+		}	
+	} // end of compareTo(IntervalADT<T> other) method
+    
+	/**
+	 * Returns properly formatted String containing Interval's label, 
+	 * start date, and end date.
+	 * 
+	 * @return String representation of an Interval
+	 */
+	@Override
+	public String toString() {
+		String s = this.label + " [" + this.start + ", " + this.end + "]";
+		return s;
+	} // end of toString() method
 	
-    @Override
-    public String toString() {
-        String s = this.label + " [" + this.start + ", " + this.end + "]";
-        return s;
-    }
-}
+} // end of Interval class
